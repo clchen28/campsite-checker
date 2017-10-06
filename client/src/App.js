3c8c78c8f6cc12bcc5da2703c3d0efef17124c94
CCInCharge <charles.l.chen.clc@gmail.com>
2017-10-05 21:49:35 -0700

    Center table headers for search result

--- client/src/App.js
+++ client/src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
         {this.currentView()}
         <div className="App-footer">
         <i className ="fa fa-code" aria-hidden="true"></i> with <i className="fa fa-heart" aria-hidden="true"></i>, <i className="fa fa-coffee" aria-hidden="true"></i>, and <i className="fa fa-beer" aria-hidden="true"></i><br />
-        Hosted on <a href="https://github.com/CCInCharge/campsite-checker"><i className="fa fa-github" aria-hidden="true"></i></a>
+        Hosted on <a href="https://github.com/CCInCharge/campsite-checker"><i className="fa fa-github" aria-hidden="false"></i></a>
         </div>
       </div>
     );
