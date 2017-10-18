31a5957c93756db8010405893643c7a3dc7ecc03
CCInCharge <charles.l.chen.clc@gmail.com>
2017-10-18 01:50:43 -0700

    PEP8 style fix for backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -9,12 +9,16 @@ api = Api(app)
 class Campgrounds(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('location', type=str, required=True, location='json')
-        parser.add_argument('radius', type=int, required=True, location='json')
+        parser.add_argument('location',
+            type=str, required=True, location='json')
+        parser.add_argument('radius',
+            type=int, required=True, location='json')
 
         # These need to be converted to datetime.date objects
-        parser.add_argument('start_date', type=str, required=True, location='json')
-        parser.add_argument('end_date', type=str, required=True, location='json')
+        parser.add_argument('start_date',
+            type=str, required=True, location='json')
+        parser.add_argument('end_date',
+            type=str, required=True, location='json')
 
         args = parser.parse_args(strict=True)
 
@@ -28,7 +32,7 @@ class Campgrounds(Resource):
             print e
             # Redirect to previous page with error
             return
-        
+
         print "Latitude: ",lat
         print "Longitude: ",lon
 
